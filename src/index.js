19c21e3c3fb161f19d2fb2358ff8860e8a7b3874
Vingo <1031839835@qq.com>
2019-06-28 11:36:10 +0800

    change back create react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import dva from 'dva';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// 1. Initialize
-const app = dva();
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// 2. Plugins
-// app.use({});
-
-// 3. Model
-// app.model(require('./models/example').default);
-
-// 4. Router
-app.router(require('./router').default);
-
-// 5. Start
-app.start('#root');
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
